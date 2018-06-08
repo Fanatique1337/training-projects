c21441873d2783993cb6e97747d9d04958e58d13
Fanatique1337 <pnikolow1337@gmail.com>
2018-06-08 11:32:34 +0300

    Added bfsh encryption to passwordsafe

--- projects/passwordsafe/passwordsafe.py
+++ projects/passwordsafe/passwordsafe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
 
-from blowfish.py import cipher
+import blowfish
 
+from os import urandom
+
+
+class TestIt(object):
+	byte_order = "big"
+
+	@classmethod
+	def setUpClass(cls):
+		cls.cipher = blowfish.Cipher(
+			b"somekey",
+			byte_order = cls.byte_order)
+
+
+	def encryptit(self, word):
+		cipher = self.cipher
+
+		iv = urandom(8)
+
+		enc = b"".join(cipher.encrypt_cfb(word, iv))
+		print(enc)
+		dec = b"".join(cipher.decrypt_cfb(enc, iv))
+		print(dec)
+
+crypt = "Test test 1 2 3".encode('utf-8')
+tester = TestIt()
+tester.setUpClass()
+tester.encryptit(crypt)
