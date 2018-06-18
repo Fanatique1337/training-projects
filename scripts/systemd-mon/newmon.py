88b3d72884e0564f30a3b188e96674cfeb9a5c12
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-06-18 18:30:02 +0300

    Beautified newmon.py and added comments

--- scripts/systemd-mon/newmon.py
+++ scripts/systemd-mon/newmon.py
@@ -5,28 +5,34 @@
 # 7	: 	TBA
 # 8	:	TBA
 
-import sys
+# Import default system libraries.
+
+import getopt
 import os
 import subprocess
-#import time
-import getopt
+import sys
+# from datetime import datetime # Uncomment if going to benchmark for speed.
+
+# Import external libraries.
+
 import psutil
-from datetime import datetime
 from pathlib import Path
-from os import listdir
-sys.tracebacklimit = 1000
-# Get arguments and set configuration
-#startTime = datetime.now()
+
+# startTime = datetime.now() # Start the timer for benchmarking. Must uncomment last line as well.
+
+# Get arguments and set configuration.
 
 def parse_args():
 	cfgfile = 'smon.conf'
 	minimal = False
+
 	if len(sys.argv) > 1:
 		try:
 			opts, args = getopt.getopt(sys.argv[1:], 'mc:', ['minimal=', 'config='])
 		except getopt.GetoptError:
 			print("An error occured while parsing your arguments. Check the proper usage of the script.")
 			sys.exit(6)
+
 		for opt, arg in opts:
 			if opt in ('-m', '--minimal'):
 				minimal = True
@@ -35,89 +41,106 @@ def parse_args():
 
 	return minimal, cfgfile
 
+# Read services from the configuration file and add them into a list.
+
 def load_services(handlerlist, cfg):
 	with open(cfg, "r") as servfile:
 		for line in servfile:
 			handlerlist.append(line.strip())
 	return handlerlist
 
+# Read CPU and Memory usage of the processes.
+
 def read_stats(ss):
 	cpud = {}
 	memd = {}
+
 	for pid in ss:
-		with open(os.path.join('/proc/', str(pid), 'stat'), 'r') as pfile:
+
+		# CPU times and usage can be found in the /proc/ filesystem in stat files.
+
+		with open(os.path.join('/proc/', str(pid), 'stat'), 'r') as pfile: 
 			pidtimes = pfile.read().split(' ')
-			#print(pidtimes)
 			pname = str(pidtimes[1])[1:-1]
-			#print(pname)
-			utime = int(pidtimes[13])
-			stime = int(pidtimes[14])
+			utime = int(pidtimes[13]) # utime is the 14th element in the stat file (man proc).
+			stime = int(pidtimes[14]) # stime is the 15th element in the stat file (man proc).
 			pidtotal = utime - stime
 
-		with open('/proc/stat', 'r') as cfile:
+		with open('/proc/stat', 'r') as cfile: # Get total system CPU times.
 			cputimes = cfile.readline().split(' ')
 			cputotal = 0
 			for integ in cputimes[2:]:
 				integ = int(integ)
 				cputotal = cputotal + integ
-		usg = (pidtotal / cputotal) * 100
-		if usg < 0:
-			usg = 0
-		cpud[pname] = str(usg)
 
-		phandler = psutil.Process(pid)
-		pmem = phandler.memory_percent()
+		usg = (pidtotal / cputotal) * 100 # Process CPU usage is process cpu times / system cpu time.
+
+		if usg < 0: # Deny negative values
+			usg = 0
+
+		cpud[pname] = str(usg) # A major bug here. Have to fix. For each PID it rewrites the usage..
+		phandler = psutil.Process(pid) # Generate a process class for the given PID.
+		pmem = phandler.memory_percent() # Get memory usage in percents of services.
 		memd[pname] = str(pmem)
 
 	return cpud, memd
 
+# Get the Process ID for each service in the configuration file. 
+
 def get_pid(slist):
-	pidchecks = []
+	pidchecks = [] # Predefine the list of PIDs.
+
 	for svc in slist:
-		cpuusage = 0
-		try:
+
+		cpuusage = 0 # Predefine the variable for CPU usage.
+
+		try: # For every service, try to find its PID file in /var/run and read it.
 			pidfpath = '/var/run/{}/{}.pid'.format(svc, svc)
-			if not Path(pidfpath).exists():
+			if not Path(pidfpath).exists(): # Most services have a /var/run/service/service.pid file.
 				pidfpath = '/var/run/{}.pid'.format(svc)
-				if not Path(pidfpath).exists():
+				if not Path(pidfpath).exists(): # Others have a /var/run/service.pid file.
 					pidfolder = '/var/run/{}'.format(svc)
-					tmpc = listdir(pidfolder)
-					for f in tmpc:
+					tmpc = os.listdir(pidfolder)
+					for f in tmpc: # And others have various pidfiles like /var/run/service/pid.
 						f = str(f)
 						if 'pid' in f:
-							pidfpath = pidfolder + '/' + f
+							pidfpath = pidfolder + '/' + f # Add the file to the dir path. 
+
 			with open(pidfpath, 'r') as pidf:
-				mainpid = int(pidf.readline().strip())
-		except Exception:
+				mainpid = int(pidf.readline().strip()) # Read the PID number. Not sure if strip is needed. Have to check.
+
+		except Exception as e: # If such a PID file does not exist, get Main PID from parsing systemctl.
 			try:
 				mainpid = int(subprocess.check_output("systemctl status {} | grep 'Main PID: ' | grep -Eo '[[:digit:]]*' | head -n 1".format(svc), shell=True))
-			except ValueError as e:
+			except ValueError as e: # If systemctl returns nothing, then such a service does not exist.
 				pass
-		try:
+
+		try: # Get all the children of the Main PID and append them to a list.
 			mainproc = psutil.Process(mainpid)
 			mchildren = mainproc.children(recursive=True)
 			pidchecks.append(mainpid)
 			for child in mchildren:
 				pidchecks.append(child.pid)
-		except psutil._exceptions.NoSuchProcess:
+		except psutil._exceptions.NoSuchProcess: # Return an error if there is no such process working.
 			print("No running process with pid {} ({}). Probably the service isn't working.\n".format(str(mainpid), svc))
-		except psutil._exceptions.ZombieProcess:
+		except psutil._exceptions.ZombieProcess: # Return an error if the process is a zombie process.
 			print("The process with pid {} ({}) is a zombie process\n".format(str(mainpid), svc))
+
 	return pidchecks
 
 def main():
-	minimal, cfg = parse_args()
-	services = []
-	services = load_services(services, cfg)
-	pidlist = get_pid(services)
-	cpudic = {}
-	memdic = {}
-	cpudic, memdic = read_stats(pidlist)
-	for (entry, usg) in cpudic.items():
+	minimal, cfg = parse_args() # Get arguments for minimal mode and for the configuration file.
+	services = [] # Predefine the services list.
+	services = load_services(services, cfg) # Get the services into the list by using the cfg file.
+	pidlist = get_pid(services) # Get PIDs of the services' processes.
+	cpudic = {} # Predefine the dictionary for CPU usage.
+	memdic = {} # Predefine the dictionary for RAM usage.
+	cpudic, memdic = read_stats(pidlist) # Get stats into the dictionary.
+	for (entry, usg) in cpudic.items(): # Print the results.
 		print("CPU usage of process {}: {}%".format(entry, usg))
 		print("Memory usage of process {}: {}%\n".format(entry, memdic[entry]))
 
-main()
+main() # No need for main module check.
 
 
-#print("Time ran: {}".format(datetime.now() - startTime))
+# print("Time ran: {}".format(datetime.now() - startTime)) # Uncomment if going to benchmark.
