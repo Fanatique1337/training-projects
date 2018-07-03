38e3081fd5089adc99312c3dd93680fb4849aff5
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-07-03 11:36:09 +0300

    Added checks to lines

--- tasks/lines/lines.py
+++ tasks/lines/lines.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+import sys
 
 inp = input("").split(" ")
 length = int(inp[0])
 a = int(inp[1])
 b = int(inp[2])
 c = int(inp[3])
-#print(inp, length, a, b, c)
+mx = 100000
+
+if length > mx or a > mx or b > mx or c > mx:
+	print("All numbers must be lower than 100 000.")
+	sys.exit(3)
 
 counter_a = 0
 counter_b = length
@@ -20,9 +25,6 @@ while(counter_a <= length):
 while(counter_b >= 0):
 	dots_b.append(counter_b)
 	counter_b -= b
-
-#print("A: {}".format(dots_a))
-#print("B: {}".format(dots_b))
 
 for dota in dots_a:
 	for dotb in dots_b:
