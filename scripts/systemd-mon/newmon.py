b0b8f888ddea377b7ae0f932053ec6acf1b0b31f
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-07-30 13:08:24 +0300

    Added diff types of memories and child fork num to debmon.py

--- scripts/systemd-mon/newmon.py
+++ scripts/systemd-mon/newmon.py
@@ -43,6 +43,10 @@ def parse_arg():
 							help="Benchmark the script's runtime",
 							action="store_true",
 							default=False)
+		parser.add_argument("--childs",
+							help="Show number of child processes for each service.",
+							action="store_true",
+							default=False)
 		args = parser.parse_args()
 
 	except argparse.ArgumentError:
@@ -50,7 +54,7 @@ def parse_arg():
 			   "Check the proper usage of the script."), file=sys.stderr)
 		sys.exit(ARGPARSE_ERR)
 
-	return args.config, args.benchmark
+	return args.config, args.benchmark, args.childs
 
 def load_services(cfg):
 	"""Read services from the configuration file and add them into a list."""
@@ -79,7 +83,7 @@ def read_stats(service_pids):
 			pname = str(pidtimes[1])[1:-1]
 
 		cpud[pname] = 0
-		memd[pname] = 0
+		memd[pname] = (0, 0, 0, 0, 0)
 
 	for pid in service_pids:
 
@@ -108,8 +112,18 @@ def read_stats(service_pids):
 		newusage = cpud[pname] + usage
 		cpud[pname] = newusage # Calculate the usage and add to it.
 		phandler = psutil.Process(pid) # Generate a process class for the given PID.
-		pmem = phandler.memory_percent() # Get memory usage in percents of services.
-		newpmem = memd[pname] + pmem
+		pmem_uss = phandler.memory_percent(memtype="uss") # Get memory usage in percents of services.
+		pmem_rss = phandler.memory_percent(memtype="rss")
+		pmem_vms = phandler.memory_percent(memtype="vms")
+		pmem_pss = phandler.memory_percent(memtype="pss")
+		pmem_swap = phandler.memory_percent(memtype="swap")
+		pmem = (pmem_uss, pmem_rss, pmem_vms, pmem_pss, pmem_swap)
+		newpmem_uss = memd[pname][0] + pmem_uss
+		newpmem_rss = memd[pname][1] + pmem_rss
+		newpmem_vms = memd[pname][2] + pmem_vms
+		newpmem_pss = memd[pname][3] + pmem_pss
+		newpmem_swap = memd[pname][4] + pmem_swap
+		newpmem = (newpmem_uss, newpmem_rss, newpmem_vms, newpmem_pss, newpmem_swap)
 		memd[pname] = newpmem
 
 	return cpud, memd
@@ -118,6 +132,7 @@ def get_pid(slist):
 	"""Get the Process ID for each service in the configuration file. """
 
 	pidchecks = [] # Predefine the list of PIDs.
+	child_num = {}
 
 	for service in slist:
 
@@ -136,8 +151,11 @@ def get_pid(slist):
 		try: # Get all the children of the Main PID and append them to a list.
 			main_proc = psutil.Process(mainpid)
 			pidchecks.append(mainpid)
+			child_num[service] = 0
 			for child in main_proc.children(recursive=True):
 				pidchecks.append(child.pid)
+				child_num[service] += 1
+
 		except psutil.NoSuchProcess: # Return an error if there is no such process working.
 			print(("No running process with pid {} ({}). "
 				   "Probably the service isn't working.\n").format(str(mainpid), service),
@@ -146,7 +164,7 @@ def get_pid(slist):
 			print("The process with pid {} ({}) is a zombie process\n".format(str(mainpid), service),
 					file=sys.stderr)
 
-	return pidchecks
+	return pidchecks, child_num
 
 def get_pidf_path(service):
 	"""Check if a pidfile exists in /run directory"""
@@ -191,13 +209,23 @@ def get_pidf_path(service):
 				return pidfpath
 
 def main():
-	cfg, benchmark = parse_arg() # Get arguments for minimal mode and for the configuration file.
+	cfg, benchmark, childs = parse_arg() # Get arguments for minimal mode and for the configuration file.
 	services = load_services(cfg) # Get the services into the list by using the cfg file.
-	pidlist = get_pid(services) # Get PIDs of the services' processes.
+	pidlist, child_nums = get_pid(services) # Get PIDs of the services' processes.
 	cpudic, memdic = read_stats(pidlist) # Get stats into the dictionary.
 	for (entry, usage) in cpudic.items(): # Print the results.
 		print("CPU usage of process {0}: {1: .2f}%".format(entry, usage))
-		print("Memory usage of process {0}: {1: .2f}%\n".format(entry, memdic[entry]))
+		print("Memory usage of process {0} (rss, uss, pss, vms, swap): {1: .2f}%, {2: .2f}%, {3: .2f}%, {4: .2f}%, {5: .2f}%\n".format(entry, 
+			memdic[entry][1], 
+			memdic[entry][0], 
+			memdic[entry][3], 
+			memdic[entry][2], 
+			memdic[entry][4]))
+
+	if childs:
+		for service in services:
+			print("Number of child processes for {0}: {1}".format(service, child_nums[service]))
+
 
 	if benchmark:
 		print("Time ran: {}".format(datetime.now() - startTime))
@@ -207,7 +235,6 @@ try:
 		startTime = datetime.now() # Start the timer for benchmarking. 
 		main()
 except Exception as err:
-	print("A global exception has occured.", file=sys.stderr)
+	print("A global exception has been caught.", file=sys.stderr)
 	print(err, file=sys.stderr)
 	sys.exit(GLOBAL_ERR)
-
