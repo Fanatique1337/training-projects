f27084caecd7b91a4cec551f86efd174b90f641e
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-07-31 12:56:50 +0300

    Fixed the benchmark scripts to be fully accurate.

--- tasks/python/redis-conn/new_redis-conn.py
+++ tasks/python/redis-conn/new_redis-conn.py
@@ -5,12 +5,12 @@ from datetime import datetime
 import timeit
 
 def timefunc():
+
 	connection = redis.StrictRedis(host='localhost', port=6379, db=0)
-	return connection.client_list()
 
 startTime = datetime.now()
 
-timeit_result = timeit.timeit("timefunc()", setup="from __main__ import timefunc", number=100)
+timeit_result = timeit.timeit("timefunc()", setup="from __main__ import timefunc", number=10000)
 
 print("Time ran without pool: {}".format(datetime.now() - startTime))
 print("Python timeit: {}".format(timeit_result))
--- tasks/python/redis-conn/pool_redis-conn.py
+++ tasks/python/redis-conn/pool_redis-conn.py
@@ -4,16 +4,15 @@ import redis
 from datetime import datetime
 import timeit
 
-pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=101)
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=10000)
 
 def timefunc():
 
 	connection = redis.StrictRedis(connection_pool=pool)
-	return connection.client_list()
 
 startTime = datetime.now()
 
-timeit_result = timeit.timeit("timefunc()", setup="from __main__ import timefunc", number=100)
+timeit_result = timeit.timeit("timefunc()", setup="from __main__ import timefunc", number=10000)
 
 print("Time ran with pool: {}".format(datetime.now() - startTime))
 print("Python timeit: {}".format(timeit_result))
