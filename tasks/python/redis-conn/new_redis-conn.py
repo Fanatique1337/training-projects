8585ec1608c231f250aa602aac99e86eca200e9c
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-08-14 10:16:41 +0300

    updated redis connection benchmark

--- tasks/python/redis-conn/new_redis-conn.py
+++ tasks/python/redis-conn/new_redis-conn.py
@@ -3,14 +3,20 @@
 import redis
 from datetime import datetime
 import timeit
+import psutil
 
-test_num = 5000
+proc = psutil.Process()
+
+test_num = 10
 
 def timefunc():
-	connection = redis.Connection(host='8.20.1.111', port=6379, db=0)
+	connection = redis.Connection(host='localhost', port=6379, db=0)
 	connection.connect()
+	print("Open connections: {}".format(len(proc.connections())))
+	connection.disconnect()
 
 timeit_result = timeit.timeit("timefunc()", setup="from __main__ import timefunc", number=test_num)
+print("Open connections after test: {}".format(len(proc.connections())))
 
 print("Timeit result (total): {}".format(timeit_result))
 print("Timeit result (average): {}".format(timeit_result / test_num))
