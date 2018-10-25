5fe94f1b5713ef146192079d9c95f1da17b04509
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-10-25 17:27:39 +0300

    Fixed esxi backup

--- scripts/esxi-backups/esxi_vm_backup.sh
+++ scripts/esxi-backups/esxi_vm_backup.sh
@@ -3,13 +3,14 @@
 # ========[ CONFIGURATION ]======== #
 
 ESXI=`/bin/cat /etc/esxi_host_addr`
-DEST="[datastore1] /backup"
+DEST=`/bin/cat /etc/esxi_vm_backup | /bin/grep destination | /usr/bin/awk -F '=' '{print $2}'`
 ESXI_USER=`/bin/cat /etc/esxi_host_auth | /bin/grep username | /usr/bin/awk -F '=' '{ print $2 }'`
 PASSWORD=`/bin/cat /etc/esxi_host_auth | /bin/grep password | /usr/bin/awk -F '=' '{ print $2 }'`
 
 # ========[      CODE     ]======== #
 
-for vm in `cat vms`;
+for VM in `/bin/cat /etc/esxi_vm_backup | /bin/grep vmname | /usr/bin/awk -F '=' '{print $2}'`;
 do
-    /usr/bin/bazaarvcb backup -H "$ESXI" -u "$ESXI_USER" -p "$PASSWORD" "$vm" "$DEST" --consolidate
+    /usr/bin/bazaarvcb backup --consolidate -H "${ESXI}" -u "${ESXI_USER}" -p "${PASSWORD}" ${VM} "${DEST}"
+    #/usr/bin/bazaarvcb backup --consolidate -H `printf "${ESXI}"` -u `printf "${ESXI_USER}"` -p `printf "${PASSWORD}"` `printf "${VM}"` `printf "${DEST}"`
 done
