9d610a60399ad076f2b56a42c4e42ad32307d654
Pavlin Nikolov <pnikolow1337@gmail.com>
2018-11-06 15:06:31 +0200

    Updated datastore-raid, need to check all the info I've collected

--- scripts/datastore-raid/datastore-raid.sh
+++ scripts/datastore-raid/datastore-raid.sh
@@ -16,5 +16,22 @@ if [ "$MOUNT_ONE" == "$MOUNT_TWO" ]; then
     exit 1
 fi
 
-DISK_ONE_FILENAME=$(/usr/share/tbmon2/bin/get_vsphereapi_data.pl --server `/bin/cat /etc/esxi_host_addr` --sessionfile /tmp/vsphereapi_session | grep 'Hard disk 2|VirtualDisk.VirtualDiskFlatVer2BackingInfo|backing.fileName:' | rev | cut -d ' ' -f 1 | rev)
-DISK_TWO_FILENAME=$(/usr/share/tbmon2/bin/get_vsphereapi_data.pl --server `/bin/cat /etc/esxi_host_addr` --sessionfile /tmp/vsphereapi_session | grep 'Hard disk 3|VirtualDisk.VirtualDiskFlatVer2BackingInfo|backing.fileName:' | rev | cut -d ' ' -f 1 | rev)
+DISK_ONE_FILENAME=$(/usr/share/tbmon2/bin/get_vsphereapi_data.pl --server `/bin/cat /etc/esxi_host_addr` --sessionfile /tmp/vsphereapi_session | grep 'Hard disk 2|VirtualDisk.VirtualDiskFlatVer2BackingInfo|backing.fileName:' | rev | cut -d ':' -f 1 | rev)
+DISK_TWO_FILENAME=$(/usr/share/tbmon2/bin/get_vsphereapi_data.pl --server `/bin/cat /etc/esxi_host_addr` --sessionfile /tmp/vsphereapi_session | grep 'Hard disk 3|VirtualDisk.VirtualDiskFlatVer2BackingInfo|backing.fileName:' | rev | cut -d ':' -f 1 | rev)
+DISK_ONE_DATASTORE=$(echo $DISK_ONE_FILENAME | cut -d ' ' -f1 | cut -d '[' -f2 | cut -d ']' -f1)
+DISK_TWO_DATASTORE=$(echo $DISK_TWO_FILENAME | cut -d ' ' -f1 | cut -d '[' -f2 | cut -d ']' -f1)
+
+echo $DISK_ONE_FILENAME
+echo $DISK_TWO_FILENAME
+echo $DISK_ONE_DATASTORE
+echo $DISK_TWO_DATASTORE
+
+DISK_ONE_DATASTORE_EXTENT=$(/bin/esxcli storage vmfs extent list | grep $DISK_ONE_DATASTORE | awk '{print $4}')
+DISK_TWO_DATASTORE_EXTENT=$(/bin/esxcli storage vmfs extent list | grep $DISK_TWO_DATASTORE | awk '{print $4}')
+
+echo $DISK_ONE_DATASTORE_EXTENT
+echo $DISK_TWO_DATASTORE_EXTENT
+
+DISK_ONE_RAID_LEVEL=$(/bin/esxcli storage core device list -d $DISK_ONE_DATASTORE_EXTENT | grep "RAID Level: " | rev | cut -d ' ' -f1 | rev)
+DISK_TWO_RAID_LEVEL=$(/bin/esxcli storage core device list -d $DISK_TWO_DATASTORE_EXTENT | grep "RAID Level: " | rev | cut -d ' ' -f1 | rev)
+
